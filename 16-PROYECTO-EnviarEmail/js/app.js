ed1a9b5de7479f6a05d6bdbb19a366eb7215a822
Davidkuguk <46162029+Davidkuguk@users.noreply.github.com>
2025-02-11 00:34:30 +0100

    gestionamos que se muestren mensajes si no se ingreso nada en el campo

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', function(){
     
     //seleccionar lo elementos de la interfaz
     const inputEmail = document.querySelector('#email');
-    const inputAsunto = document.querySelector('#asunto')
-    const inputMensaje = document.querySelector('#mensaje')
-
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
     //asignando eventos
     inputEmail.addEventListener('blur', validar);
@@ -15,10 +15,18 @@ document.addEventListener('DOMContentLoaded', function(){
 
     function validar(event){
         if(event.target.value.trim() === ''){
-            console.log('Esta vacio');
+            mostrarAlerta(`el campo ${event.target.id} es obligatorio`);
         }else{
             console.log('si hay algo');
         }
+    }
+
+    function mostrarAlerta(mensaje){
+        //alertamos por el error
+        const error = document.createElement('p');
+        error.textContent = mensaje;
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center')
+        formulario.appendChild(error)
     }
 
 
