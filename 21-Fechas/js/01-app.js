200545e19f41dd01f0965b599c89b4acb39f9f93
Davidkuguk <46162029+Davidkuguk@users.noreply.github.com>
2025-02-19 19:37:18 +0100

    inicio metodos date()

---
+++ 21-Fechas/js/01-app.js
@@ -0,0 +1,15 @@
+const diaHoy = new Date();
+
+//metodos Date()
+
+let valor;
+
+valor = diaHoy;
+valor = diaHoy.getFullYear();
+valor = diaHoy.getMonth();
+valor = diaHoy.getHours();
+valor = diaHoy.getMinutes();
+valor = diaHoy.getSeconds();
+
+
+console.log( valor);
