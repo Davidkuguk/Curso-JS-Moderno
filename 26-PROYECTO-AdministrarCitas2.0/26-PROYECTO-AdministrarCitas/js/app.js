c6bb93094050600a23ccbb59f631465e6be19391
Davidkuguk <46162029+Davidkuguk@users.noreply.github.com>
2025-03-12 20:53:30 +0100

    mostramos alertas, añadimos el objeto a la interfaz

--- 26-PROYECTO-AdministrarCitas2.0/26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas2.0/26-PROYECTO-AdministrarCitas/js/app.js
@@ -5,6 +5,7 @@ const emailInput = document.querySelector('#email');
 const fechaInput = document.querySelector('#fecha');
 const sintomasInput = document.querySelector('#sintomas');
 const formulario = document.querySelector('#formulario-cita');
+const contenedorCitas = document.querySelector('#citas')
 
 //objeto de cita
 const citaObj = {
@@ -34,24 +35,8 @@ sintomasInput.addEventListener('change', datosCita)
 formulario.addEventListener('submit', submitCita);
 
 /**
- *Funcion dinamica para que recupere los valores
- del formulario y los escriba en el obj
+ * Classes
  */
-function datosCita(e){
-    citaObj[e.target.name] = e.target.value;
-    console.log(citaObj);
-}
-
-function submitCita(e){
-    e.preventDefault();
-    
-    if(Object.values(citaObj).some(valor => valor.trim() === '')){
-        new Notificacion('Todos los campos son obligatorios', 'error');
-        return;
-    }
-
-}
-
 class Notificacion {
     constructor(texto,tipo){
         this.texto = texto;
@@ -88,6 +73,94 @@ class Notificacion {
 class AdminCitas{
     constructor(){
         this.citas =  [];
-        console.log();
     }
+
+    agregar(cita){
+        this.citas = [...this.citas, cita]
+        this.mostrar()
+    }
+
+    mostrar(){
+        //limpiarmos el html
+        while(contenedorCitas.firstChild){
+            contenedorCitas.removeChild(contenedorCitas.firstChild)
+        }
+
+        this.citas.forEach(cita => {
+            const divCita = document.createElement('div');
+            divCita.classList.add('mx-5', 'my-10', 'bg-white', 'shadow-md', 'px-5', 'py-10' ,'rounded-xl', 'p-3');
+        
+            const paciente = document.createElement('p');
+            paciente.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
+            paciente.innerHTML = `<span class="font-bold uppercase">Paciente: </span> ${cita.paciente}`;
+        
+            const propietario = document.createElement('p');
+            propietario.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
+            propietario.innerHTML = `<span class="font-bold uppercase">Propietario: </span> ${cita.propietario}`;
+        
+            const email = document.createElement('p');
+            email.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
+            email.innerHTML = `<span class="font-bold uppercase">E-mail: </span> ${cita.email}`;
+        
+            const fecha = document.createElement('p');
+            fecha.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
+            fecha.innerHTML = `<span class="font-bold uppercase">Fecha: </span> ${cita.fecha}`;
+        
+            const sintomas = document.createElement('p');
+            sintomas.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
+            sintomas.innerHTML = `<span class="font-bold uppercase">Síntomas: </span> ${cita.sintomas}`;
+        
+            const btnEditar = document.createElement('button');
+btnEditar.classList.add('py-2', 'px-10', 'bg-indigo-600', 'hover:bg-indigo-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
+btnEditar.innerHTML = 'Editar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
+
+const btnEliminar = document.createElement('button');
+btnEliminar.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
+btnEliminar.innerHTML = 'Eliminar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+            // Agregar al HTML
+            divCita.appendChild(paciente);
+            divCita.appendChild(propietario);
+            divCita.appendChild(email);
+            divCita.appendChild(fecha);
+            divCita.appendChild(sintomas);
+            contenedorCitas.appendChild(divCita);
+        });  
+    }
+}
+
+
+/**
+ *Funcion dinamica para que recupere los valores
+ del formulario y los escriba en el obj
+ */
+function datosCita(e){
+    citaObj[e.target.name] = e.target.value;
+    console.log(citaObj);
+}
+
+const citas = new AdminCitas();
+
+function submitCita(e){
+    e.preventDefault();
+    
+    if(Object.values(citaObj).some(valor => valor.trim() === '')){
+        new Notificacion('Todos los campos son obligatorios', 'error');
+        return;
+    }
+
+    citas.agregar({...citaObj})
+    formulario.reset()
+    reiniciarObjetoCita()
+
+    new Notificacion('Paciente Registrado', 'exito')
+}
+
+function reiniciarObjetoCita(){
+    Object.assign(citaObj, {
+        paciente: '',
+        propietario: '',
+        email: '',
+        fecha: '',
+        sintomas: ''
+    })
 }
