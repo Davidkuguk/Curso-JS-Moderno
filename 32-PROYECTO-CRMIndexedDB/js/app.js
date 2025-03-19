4ea6e30efd34829c73ac71f91eb0db8a15bdd37e
Davidkuguk <46162029+Davidkuguk@users.noreply.github.com>
2025-03-19 22:39:18 +0100

    asignamos la bbdd de nuestro proyecto, tambien creamos las columnas de la bbdd

---
+++ 32-PROYECTO-CRMIndexedDB/js/app.js
@@ -0,0 +1,36 @@
+(function(){
+
+    let DB;
+
+    document.addEventListener('DOMContentLoaded', () =>{
+        crearDB();
+
+        //crear la bbdd
+        function crearDB(){
+            const crearDB = window.indexedDB.opnes('crm', 1);
+
+            crearDB.oneError = function(){
+                console.log('Hubo un error');
+            }
+
+            crearDB.onsuccess = function(){
+                console.log('ejecutando bbdd');
+                DB = crearDB.result;
+            }
+
+            crearDB.onupgradeneeded = function(e){
+                const db = e.target.result;
+                const objectStore = db.createObjectStore('crm', {keyPath: 'id', autoIncrement: True})
+                objectStore.createIndex('nombre', 'nombre', {unique: false});
+                objectStore.createIndex('email', 'email', {unique: true});
+                objectStore.createIndex('telefono', 'telefono', {unique: false});
+                objectStore.createIndex('Empresa', 'Empresa', {unique: false});
+                objectStore.createIndex('id', 'id', {unique: true})
+
+                console.log('DB lista y creada');
+            }
+
+        }
+    })
+})();
+
