d4be5fb7c7b9155e02f3213cf158c244595f46d7
Davidkuguk <46162029+Davidkuguk@users.noreply.github.com>
2025-03-19 22:41:09 +0100

    debuggin

--- 32-PROYECTO-CRMIndexedDB/js/app.js
+++ 32-PROYECTO-CRMIndexedDB/js/app.js
@@ -20,7 +20,7 @@
 
             crearDB.onupgradeneeded = function(e){
                 const db = e.target.result;
-                const objectStore = db.createObjectStore('crm', {keyPath: 'id', autoIncrement: True})
+                const objectStore = db.createObjectStore('crm', {keyPath: 'id', autoIncrement: true})
                 objectStore.createIndex('nombre', 'nombre', {unique: false});
                 objectStore.createIndex('email', 'email', {unique: true});
                 objectStore.createIndex('telefono', 'telefono', {unique: false});
